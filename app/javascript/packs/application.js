830d5a67f0cb9db8645f3c9fd54f2e862137a9ac
Thiago Araujo <worf.mogh@gmail.com>
2019-12-02 22:05:35 -0800

    bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'bootstrap'
+import './src/application.scss'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -15,3 +17,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
